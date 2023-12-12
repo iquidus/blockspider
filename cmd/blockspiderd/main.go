bac2bbf2722d9c7cd007f36faa0b9ea31e9a87fc
iquidus <luke@iquidus.io>
2023-12-12 14:42:36 -0500

    blockspiderd: move startCrawler() to main.go; remove crawler.go

--- cmd/blockspiderd/main.go
+++ cmd/blockspiderd/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/iquidus/blockspider/common"
+	"github.com/iquidus/blockspider/crawler"
 	"github.com/iquidus/blockspider/disk"
 	"github.com/iquidus/blockspider/params"
 	"github.com/iquidus/blockspider/state"
@@ -131,4 +132,10 @@ func main() {
 
 	quit := make(chan int)
 	<-quit
+}
+
+func startCrawler(cfg *crawler.Config, s *state.State, rpc *common.RPCClient, logger log.Logger) {
+	blockCrawler := crawler.NewCrawler(cfg, s, rpc, logger.New())
+	logger.Info("Starting crawler")
+	crawler.Start(blockCrawler, cfg, logger)
 }
